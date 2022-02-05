ef6e440c974037e45aa609ce1437e099883bb624
tkuti <tunde.kuti@gmail.com>
2022-02-05 16:41:07 +0100

    PaginationBar

---
+++ components/PaginationBar.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect, useContext } from 'react'
+import { Pagination } from 'react-bootstrap'
+import { DataContext } from '../context/dataContext'
+
+const PaginationBar = () => {
+  const { page, setPage } = useContext(DataContext)
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    createBar()
+  }, [page])
+
+  const createBar = () => {
+    const pagination = []
+    pagination.push(<Pagination.First onClick={() => setPage(1)} />)
+    pagination.push(
+      <Pagination.Prev onClick={() => page !== 1 && setPage(page - 1)} />
+    )
+    pagination.push(
+      <Pagination.Ellipsis onClick={() => page > 11 && setPage(page - 10)} />
+    )
+    const floor = Math.floor(page / 10) * 10 || 1
+    for (let number = floor; number <= floor + 9; number++) {
+      pagination.push(
+        <Pagination.Item
+          key={number}
+          active={number === page}
+          value={number}
+          onClick={() => setPage(number)}
+        >
+          {number}
+        </Pagination.Item>
+      )
+    }
+    pagination.push(
+      <Pagination.Ellipsis onClick={() => page < 792 && setPage(page + 10)} />
+    )
+    pagination.push(
+      <Pagination.Next onClick={() => page !== 802 && setPage(page + 1)} />
+    )
+    pagination.push(<Pagination.Last onClick={() => setPage(802)} />)
+    setItems(pagination)
+  }
+
+  const paginationBasic = (
+    <div>
+      <Pagination>{items}</Pagination>
+    </div>
+  )
+
+  return <div>{paginationBasic}</div>
+}
+
+export default PaginationBar
--- pages/breweries.js
+++ pages/breweries.js
@@ -3,6 +3,7 @@ import { DataContext } from '../context/dataContext'
 import Table from 'react-bootstrap/Table'
 import Spinner from 'react-bootstrap/Spinner'
 import Link from 'next/link'
+import PaginationBar from '../components/PaginationBar'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const Breweries = () => {
@@ -14,34 +15,37 @@ const Breweries = () => {
       {isLoading ? (
         <Spinner animation='border' variant='primary' />
       ) : (
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Country</th>
-              <th>Type</th>
-              <th>Web</th>
-            </tr>
-          </thead>
-          <tbody>
-            {breweryList &&
-              breweryList.map((brewery, index) => (
-                <tr key={index}>
-                  <td>
-                    <Link
-                      href='/brewery/[breweryId]'
-                      as={`/brewery/${brewery.id}`}
-                    >
-                      {brewery.name}
-                    </Link>
-                  </td>
-                  <td>{brewery.country}</td>
-                  <td>{brewery.brewery_type}</td>
-                  <td>{brewery.website_url}</td>
-                </tr>
-              ))}
-          </tbody>
-        </Table>
+        <>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Country</th>
+                <th>Type</th>
+                <th>Web</th>
+              </tr>
+            </thead>
+            <tbody>
+              {breweryList &&
+                breweryList.map((brewery, index) => (
+                  <tr key={index}>
+                    <td>
+                      <Link
+                        href='/brewery/[breweryId]'
+                        as={`/brewery/${brewery.id}`}
+                      >
+                        {brewery.name}
+                      </Link>
+                    </td>
+                    <td>{brewery.country}</td>
+                    <td>{brewery.brewery_type}</td>
+                    <td>{brewery.website_url}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </Table>
+          <PaginationBar />
+        </>
       )}
     </div>
   )
